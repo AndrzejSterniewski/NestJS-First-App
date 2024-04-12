f088d155351d5008c197fdd6358fc5da27418469
Andrzej Sterniewski <drapiszon@gmail.com>
2024-04-12 12:55:03 +0200

    add new orders controllers and services, add dtos for orders

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, 
+  Get, 
+  Delete,
+  Post,
+  Put,
+  Param, 
+  Body,
+  ParseUUIDPipe, 
+  NotFoundException } from '@nestjs/common';
 import { OrdersService } from './orders.service';
+import { CreateOrderDTO } from './dtos/create-order.dto';
+import { UpdateOrderDTO } from './dtos/update-order.dto';
 
 @Controller('orders')
 export class OrdersController {
@@ -8,12 +18,46 @@ export class OrdersController {
 
     @Get('/')
     getAll(): any {
-    return this.ordersService.getAll();
+        return this.ordersService.getAll();
     }
 
+    // @Get('/:id')
+    // getById(@Param('id') id: string): any {
+    //     return this.ordersService.getById(id);
+    // }
+    
     @Get('/:id')
-    getById(@Param('id') id: string): any {
-    return this.ordersService.getById(id);
+    getById(@Param('id', new ParseUUIDPipe()) id: string) {
+      const order = this.ordersService.getById(id);
+      if (!order) throw new NotFoundException('Order not found');
+      return order;
     }
 
+    @Delete('/:id')
+    deleteById(
+        @Param('id', new ParseUUIDPipe()) id: string) {
+            if (!this.ordersService.getById(id))
+            throw new NotFoundException('Product not found');
+            
+            this.ordersService.deleteById(id);
+            return { success: true };
+    }
+
+    @Post('/')
+    create(
+        @Body() orderData: CreateOrderDTO) {
+            return this.ordersService.create(orderData);
+    }
+
+    @Put('/:id')
+    update(
+        @Param('id', new ParseUUIDPipe()) id: string,
+        @Body() orderData: UpdateOrderDTO,
+     ) {
+        if (!this.ordersService.getById(id))
+            throw new NotFoundException('Order not found');
+        
+        this.ordersService.update(id, orderData);
+        return { success: true };
+    }
 }
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Get, Delete, Post, Param, Body } from '@nestjs/common';
+import { 
+    Controller, 
+    Get, 
+    Delete, 
+    Post,
+    Put, 
+    Param, 
+    Body,
+    ParseUUIDPipe,
+    NotFoundException } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDTO } from './dtos/create-product.dto';
+import { UpdateProductDTO } from './dtos/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -13,18 +23,37 @@ export class ProductsController {
     }
 
     @Get('/:id')
-    getById(@Param('id') id: string) {
-    return this.productsService.getById(id);
+    getById(@Param('id', new ParseUUIDPipe()) id: string) {
+      const prod = this.productsService.getById(id);
+      if (!prod) throw new NotFoundException('Product not found');
+      return prod;
     }
 
     @Delete('/:id')
-    deleteById(@Param('id') id: string) {
-    this.productsService.deleteById(id);
-    return { success : true };
+    deleteById(
+        @Param('id', new ParseUUIDPipe()) id: string) {
+            if (!this.productsService.getById(id))
+            throw new NotFoundException('Product not found');
+            
+            this.productsService.deleteById(id);
+            return { success: true };
     }
 
     @Post('/')
-    create(@Body() productData: CreateProductDTO) {
-        return this.productsService.create(productData);
+    create(
+        @Body() productData: CreateProductDTO) {
+            return this.productsService.create(productData);
+    }
+
+    @Put('/:id')
+    update(
+        @Param('id', new ParseUUIDPipe()) id: string,
+        @Body() productData: UpdateProductDTO,
+     ) {
+        if (!this.productsService.getById(id))
+            throw new NotFoundException('Product not found');
+        
+        this.productsService.update(id, productData);
+        return { success: true };
     }
 }
